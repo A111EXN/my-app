e66d24d6e80fd3489395629d32f98d8dd4149362
A111EXN <a.ngan@hotmail.co.uk>
2023-03-13 13:56:32 +0000

    preparing to lanch auth firebase

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAZrzgujdwKSCkRKi8rYSgFv-SPnj4ebIA",
+  authDomain: "fir-auth-e1427.firebaseapp.com",
+  projectId: "fir-auth-e1427",
+  storageBucket: "fir-auth-e1427.appspot.com",
+  messagingSenderId: "587549092187",
+  appId: "1:587549092187:web:7c3b08963336bde900927e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app)
