020c8a43f61524ccbf2a6bb073efd69fbba3309e
A111EXN <a.ngan@hotmail.co.uk>
2023-03-14 14:15:32 +0000

    linked the app with fire store.

--- screens/EditAccountScreen.js
+++ screens/EditAccountScreen.js
@@ -1,38 +1,70 @@
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
 import { useNavigation } from '@react-navigation/native';
+import { collection, doc, setDoc, addDoc } from "firebase/firestore"; 
+import { db } from '../firebase';
 
 const EditAccountScreen = () => {
 
+  const [email,setEmail] =useState ('')
   const [username,setUsername] = useState ('')
   const [nickname,setNickname] = useState ('')
   const [age,setAge] = useState ('')
 
   const navigation = useNavigation()
 
+  function create () {
+    setDoc(doc(db, "user","test"), {
+      email: email,
+      username: username,
+      nickname: nickname,
+      age: age, 
+    }).then(()=>{
+      console.log('succsses')
+    }).catch((error)=>{
+      console.log('error');
+    });;
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
       <TextInput
-      placeholder='Enter New Username'
+      placeholder='Enter Email'
+      value={email}
       style={styles.input}
-      onChangeText={text  => setUsername(text)}
+      onChangeText={(email)  => setEmail(email)}
+      />      
+      <TextInput
+      placeholder='Enter New Username'
+      value={username}
+      style={styles.input}
+      onChangeText={(username)  => setUsername(username)}
       />
       <TextInput
       placeholder='Enter New Nickname'
+      value={nickname}
       style={styles.input}
-      onChangeText={text  => setNickname(text)}
+      onChangeText={(nickname)  => setNickname(nickname)}
       />
       <TextInput
       placeholder='Enter New Age'
+      value={age}
       style={styles.input}
-      onChangeText={text  => setAge(text)}
+      onChangeText={(age)  => setAge(age)}
       />
       </View>
       <TouchableOpacity
       style={styles.button}
+      onPress={create}
       >
-        <Text style={styles.buttonText} onPress={()=>{navigation.navigate("Login")}}>Save & Return</Text>
+        <Text style={styles.buttonText}>Save Data</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+      style={styles.button}
+      onPress={()=>{navigation.navigate("Login")}}
+      >
+      <Text style={styles.buttonText}>Return</Text>
       </TouchableOpacity>
     </View>
   )
